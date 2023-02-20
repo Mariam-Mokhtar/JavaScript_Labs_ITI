becb08d670c69a35847339250b8d7caa0f5d9a9a
Mariam Mokhtar <mariamreda1819@gmail.com>
2023-02-20 03:13:03 +0200

    update

--- workShop/Ajax/script.js
+++ workShop/Ajax/script.js
@@ -1,6 +1,3 @@
-document.querySelector('.center').style.height = `${window.innerHeight}px`;
-document.querySelector('.center').overflowY="scroll"
-
 var rowDiv=document.querySelector(".row")
 async function getData() {
     var response = await fetch('https://reqres.in/api/users?page=2');
@@ -41,13 +38,3 @@ getData().then(data => {
         rowDiv.appendChild(outerDiv);
     }
 })
-
-// <div class="col-sm-4">
-// <div class="card" style="width: 18rem;">
-//     <img src="..." class="card-img-top" alt="...">
-//     <div class="card-body">
-//       <h5 class="card-title">Card title</h5>
-//       <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-//     </div>
-//   </div>
-// </div>
